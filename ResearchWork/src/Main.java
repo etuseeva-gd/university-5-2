a7671beb7b789a49c002e102be25a6d68617a5c4
lena <lenok-cotik@yandex.ru>
2018-04-10 02:21:43 +0300

    ...

---
+++ ResearchWork/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader("g2.txt"))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+
+                System.out.println(Arrays.deepToString(Utils.parseGraph(line)));
+            }
+        }
+    }
+}
---
+++ ResearchWork/src/Utils.java
@@ -0,0 +1,34 @@
+public class Utils {
+
+    static public int[][] parseGraph(String strGraph) {
+        StringBuilder stringBuilderMatrix = new StringBuilder();
+        for (int i = 1; i < strGraph.length(); i++) {
+            int number = Utils.parseChar(strGraph.charAt(i));
+
+            StringBuilder str = new StringBuilder();
+            str.append(Integer.toBinaryString(number));
+            while (str.length() < 6) {
+                str.reverse().append("0").reverse();
+            }
+
+            stringBuilderMatrix.append(str);
+        }
+        String stringMatrix = String.valueOf(stringBuilderMatrix);
+
+        int vertexes = Utils.parseChar(strGraph.charAt(0));
+        int[][] matrix = new int[vertexes][vertexes];
+        int index = 0;
+        for (int i = 1; i < vertexes; i++) {
+            for (int j = 0; j < i; j++, index++) {
+                matrix[i][j] = Integer.parseInt(stringMatrix.substring(index, index + 1));
+                matrix[j][i] = Integer.parseInt(stringMatrix.substring(index, index + 1));
+            }
+        }
+        return matrix;
+    }
+
+    static private int parseChar(char c) {
+        return ((int) c) - 63;
+    }
+
+}
