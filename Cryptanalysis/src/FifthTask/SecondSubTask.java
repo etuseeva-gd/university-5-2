8399ad53322e4f999e34de96820a2249e9374f6f
Tuseeva Elena <etuseeva@griddynamics.com>
2018-04-14 14:37:04 +0400

    ...

--- Cryptanalysis/src/FifthTask/SecondSubTask.java
+++ Cryptanalysis/src/FifthTask/SecondSubTask.java
@@ -1,6 +1,9 @@
 package FifthTask;
 
 public class SecondSubTask {
+    /**
+     * Вычисление значений гипотезы Н(d) с наиболее вероятной длиной периода d
+     */
     public void init() {
 
     }
