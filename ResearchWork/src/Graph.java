959264500cf6d9b163ba42a71e149943a9f6da77
lena <lenok-cotik@yandex.ru>
2018-04-15 13:52:21 +0300

    ...

--- ResearchWork/src/Graph.java
+++ ResearchWork/src/Graph.java
@@ -2,16 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Graph {
-    List<List<Integer>> vertexes = new ArrayList<>();
+    private List<List<Integer>> vertexes = new ArrayList<>();
 
     Graph(int[][] matrix) {
         int n = matrix.length;
-        for (int i = 0; i < n; i++) {
-            vertexes.add(new ArrayList<>());
-            for (int j = 0; j < n; j++) {
 
+        for (int[] mItem : matrix) {
+            vertexes.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    vertexes.get(i).add(j);
+                    vertexes.get(j).add(i);
+                }
             }
         }
     }
 
+    public List<List<Integer>> getVertexes() {
+        return vertexes;
+    }
 }
